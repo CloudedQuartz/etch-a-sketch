b1a6ef6c06227d3be38ddc4cded58d5a858139a7
CloudedQuartz <ayan.pande06@gmail.com>
2024-07-13 08:44:37 +0530

    Add listeners to resize and darken elements
    - also finish up ui
    
    Signed-off-by: CloudedQuartz <ayan.pande06@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+function darkenElement(e) {
+    console.log(e.target.style.opacity)
+    e.target.style.opacity = String(Number(getComputedStyle(e.target).opacity) - 0.2)
+}
+
 function generateGrid(gridSize) {
     gridSquareSize = 960 / gridSize
     gridElem = document.createElement('div')
@@ -5,12 +10,27 @@ function generateGrid(gridSize) {
     gridElem.style.height = gridSquareSize + 'px'
     gridElem.style.width = gridSquareSize + 'px'
     container = document.getElementById("grid-container")
+    container.innerHTML = ''
 
     for (let i = 0; i < 960; i += gridSquareSize) {
         for (let j = 0; j < 960; j += gridSquareSize) {
-            container.appendChild(gridElem.cloneNode());
+            let toAppendElem = gridElem.cloneNode()
+            toAppendElem.onmouseover = (e) => darkenElement(e)
+            container.appendChild(toAppendElem);
         }
     }
 }
 
-generateGrid(16)
+function generateGridFromInput() {
+    inputElem = document.getElementById("grid-size-input")
+    console.lig
+    generateGrid(Number(inputElem.value))
+}
+
+function resizeGridListener() {
+    resizeButton = document.getElementById('grid-resize')
+    resizeButton.onclick = generateGridFromInput
+}
+
+resizeGridListener()
+generateGridFromInput()
